5374a39082affa3109082cc5d41238cdfe903274
caoanhquan-qn <caoanhquan_qn@yahoo.com>
2025-06-29 22:53:35 +0700

    add .env.template for frontend

--- frontend/src/api/base.ts
+++ frontend/src/api/base.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
-let token;
 
 const baseAPI = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:4000/api",
   headers: {
     "Content-Type": "application/json",
   },
